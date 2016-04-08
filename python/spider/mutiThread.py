3510f9a288b466604bdce2370c40668bf1bc44fb
wokly <woklywkx@gmail.com>
2016-04-08 20:53:44 +0800

    test ptcharm pust

--- python/spider/mutiThread.py
+++ python/spider/mutiThread.py
@@ -6,26 +6,28 @@ import urllib2
 import re
 
 
-startPage=1500
-endPage=1600
+startPage=1600
+endPage=1700
 condition = 200
 
 #爬取页码范围
 #Producer thread
 class Producer(threading.Thread):
-    def __init__(self, t_name, queue):
+    def __init__(self, t_name, queue,pagequeue):
         threading.Thread.__init__(self, name=t_name)
         self.urls=queue
+        self.pages=pagequeue
     def run(self):
-        for page in range(startPage,endPage):
-
-            myUrl ='http://jandan.net/ooxx/page-'+str(page)+'#comments'
+        while not self.pages.empty():
+            page=self.pages.get(1,2)
+            headersversion='Mozilla/4.0 (compatible; MSIE 5.'+self.getName()+'; Windows NT) '
+            myUrl ='http://jandan.net/aabb/page-'+str(page)+'#comments'
             #请求头域
             req = urllib2.Request(myUrl, headers={
                 'Connection': 'Keep-Alive',
                 'Accept': 'text/html, application/xhtml+xml, */*',
                 'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-             "User-Agent":'Mozilla/4.2 (compatible; MSIE 5.6; Windows NT)'
+             "User-Agent":headersversion
             })
             myResponse = urllib2.urlopen(req)
             myPage = myResponse.read()
@@ -37,7 +39,7 @@ class Producer(threading.Thread):
             contents = re.findall(reg_content,unicodePage,re.S)
             supports = re.findall(reg_support,unicodePage)
 
-            print '正在处理第'+str(page)+'页。公有'+str(len(contents))+'个链接'
+            print '正在处理第'+str(page)+'页。共有'+str(len(contents))+'个链接'
             num=0
 
             for i in range(len(supports)-1):
@@ -53,23 +55,25 @@ class Producer(threading.Thread):
                     self.urls.put(imgurls)
                     num+=1
             print '符合condition的url有'+str(num)+'个'
-            time.sleep(1)
+            time.sleep(2)
 
 #Consumer thread
+
+
 class Consumer(threading.Thread):
     def __init__(self, t_name, queue):
         threading.Thread.__init__(self, name=t_name)
-        self.urls=queue
+        self.urls = queue
+
     def run(self):
         num=0
         headersversion='Mozilla/4.1 (compatible; MSIE 5.'+self.getName()+'; Windows NT) '
         while 1:
             imgurl=self.urls.get(1.10)
-            print "线程"+self.getName()+": num"
+
             url = imgurl[0]
             page = imgurl[1]
             num = imgurl[2]
-            print url[0]
             if url[0].endswith('gif'):
                 filetype = '.gif'
             else:
@@ -91,21 +95,35 @@ class Consumer(threading.Thread):
                         imgFile.close()
                         littlenum+= 1
                         time.sleep(1)
+
+            print "线程"+self.getName()+ str(num)
+            print str(page)+'-'+str(num)+'-'+str(littlenum)
             num=num+1
 
 #Main thread
 def main():
+    pagequeue = Queue()
     queue = Queue()
-    producer = Producer('Pro.', queue)
-    producer.start()
+    for page in range(startPage,endPage):
+        pagequeue.put(page)
 
+    producers=[]
     consumers=[]
-    for i in range(5):
+
+    for i in range(3):
+        producer=Producer(i,queue,pagequeue)
+        producer.start()
+        producers.append(producer)
+
+
+    for i in range(6):
         consumer=Consumer(i,queue)
         consumer.start()
         consumers.append(consumer)
-    producer.join()
-    for i in range(5):
+
+    for i in range(3):
+        producers[i].join
+    for i in range(6):
         consumers[i].join
     print 'All threads terminate!'
 
---
+++ python/spider/simpleThread03.py
@@ -0,0 +1,59 @@
+#coding=utf-8
+#使用条件变量Condition
+__author__ = 'wen'
+import threading
+import time
+con = threading.Condition()
+x=0
+class Producer(threading.Thread):
+    def __init__(self,t_name):
+        threading.Thread.__init__(self,name=t_name)
+
+    def run(self):
+        global x
+        con.acquire()
+        if x>0:
+            con.wait()
+        else:
+            for i in range(5):
+                x=x+1
+                print 'producing...'+str(x)
+            con.notify()
+        print x
+        con.release()
+class Consumer(threading.Thread):
+    def __init__(self,t_name):
+        threading.Thread.__init__(self,name=t_name)
+    def run(self):
+        global x
+        con.acquire()
+        if x ==0:
+            print"consumer wait..."
+            con.wait()
+        else:
+            for i in range(5):
+                x=x-1
+                print 'consuming...'+str(x)
+            con.notify()
+        print x
+        con.release()
+
+print 'start consumer'
+
+c=Consumer('consumer')
+
+print 'start producer'
+
+p=Producer('producer')
+
+
+
+p.start()
+
+c.start()
+
+p.join()
+
+c.join()
+
+print x
